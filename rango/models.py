136695014ef2b77e96eaf9748cbdb549b12cebef
vaibhav-rbs <vaibhav.rbs@gmail.com>
2015-02-24 00:20:18 -0500

    completed chapter7

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.name
@@ -12,7 +14,6 @@ class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
-    views = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.title
